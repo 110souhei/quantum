b74dba9a1c738e06b954c74be178d5bef1965aed
110souhei <souhei103@gmail.com>
2024-12-11 02:57:03 +0900

    png

--- Hadama_test/sol/quantum_test/Hadamard_test.py
+++ Hadama_test/sol/quantum_test/Hadamard_test.py
@@ -2,9 +2,11 @@ from qiskit import QuantumCircuit
 import numpy as np
 
 
-def Hadamard_test(n) -> QuantumCircuit:
+def Hadamard_test(n,the,phi,lam,gam,im) -> QuantumCircuit:
     qc = QuantumCircuit(n+1)
     qc.h(0)
-    qc.cu(0,0,0,0,0,1)
+    if(im == True):
+        qc.p(np.pi*(3/2),0)
+    qc.cu(the,phi,lam,gam,0,1)
     qc.h(0)
     return qc
